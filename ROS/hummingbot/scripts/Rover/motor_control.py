3de37761f0ca6db04660d696b33c02e0f1ce360e
md <mustafa-durmuss@outlook.com>
2019-08-23 18:36:01 +0300

    updated motor-control code

--- ROS/hummingbot/scripts/Rover/motor_control.py
+++ ROS/hummingbot/scripts/Rover/motor_control.py
@@ -3,38 +3,79 @@
 # Author : Mustafa Durmuş [mustafa@hummingdrone.co]
 
 import rospy
-from std_msgs.msg import Float32
+from Adafruit_MotorHAT import Adafruit_MotorHAT
+from std_msgs.msg import String, Float32
 
 NODE_NAME = "motor_control"
 SUB_TOPIC_NAMES = ["/gazebo_hummingbot/left_vel",
                    "/gazebo_hummingbot/right_vel"]
 
 
-def set_motor_velocity(speed, direction):
+# stops all motors
+def all_stop():
+
+    motor_left.setSpeed(0)
+    motor_right.setSpeed(0)
+
+    motor_left.run(Adafruit_MotorHAT.RELEASE)
+    motor_right.run(Adafruit_MotorHAT.RELEASE)
+
+
+def set_motor_speed(speed, motor_id):
     """
-    Takes a float number and sets motor velocity of any wheel.
+    Takes a float number and sets motor speed of any wheel.
 
     speed   : Velocity of the wheel that taken from joystick
 
-    direction : Wheel name (left or right)
+    motor_id : ID of left or right wheel
 
     """
-    if direction == "left":
-        pass
+    if motor_id == 1:
+        motor = motor_left
     else:
-        pass
+        motor = motor_right
+
+    motor.setSpeed(speed)
+
+    if speed > 0:
+        motor.run(Adafruit_MotorHAT.FORWARD)
+    else:
+        motor.run(Adafruit_MotorHAT.BACKWARD)
 
 
-def callback_left(vel):
-    set_motor_velocity(vel, direction="left")
+def callback_left(velocity):
+    set_motor_speed(velocity.data, motor_id=1)
 
 
-def callback_right(vel):
-    set_motor_velocity(vel, direction="right")
+def callback_right(velocity):
+    set_motor_speed(velocity.data, motor_id=2)
 
+
+# initialization
 if __name__ == '__main__':
-    rospy.init_node(NODE_NAME, anonymous=True)
-    
-    sub_left  = rospy.Subscriber(SUB_TOPIC_NAMES[0], Float32, callback_left)
-    sub_right = rospy.Subscriber(SUB_TOPIC_NAMES[1], Float32, callback_right)
+
+    # setup motor controller
+    motor_driver = Adafruit_MotorHAT(i2c_bus=1)
+
+    # initialize id for left and right motor
+    motor_left_ID  = 1
+    motor_right_ID = 2
+
+    # get motor drivers
+    motor_left =  motor_driver.getMotor(motor_left_ID)
+    motor_right = motor_driver.getMotor(motor_right_ID)
+
+    # stop the motors as precaution
+    all_stop()
+
+    # setup ros node
+    rospy.init_node(NODE_NAME)
+
+    rospy.Subscriber(SUB_TOPIC_NAMES[0], Float32, callback_left)
+    rospy.Subscriber(SUB_TOPIC_NAMES[1], Float32, callback_right)
+
+    # start running
     rospy.spin()
+
+    # stop motors before exiting
+    all_stop()
